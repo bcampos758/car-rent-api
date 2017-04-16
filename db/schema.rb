3c36a03abba17b1bc744726187ff069c3203b1d0
acpbm <acpbm0@gmail.com>
2017-04-15 22:19:36 -0400

    Add location column to unit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413053853) do
+ActiveRecord::Schema.define(version: 20170413213739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170413053853) do
     t.integer  "reservation_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "location"
     t.index ["reservation_id"], name: "index_units_on_reservation_id", using: :btree
     t.index ["vehicle_id"], name: "index_units_on_vehicle_id", using: :btree
   end
