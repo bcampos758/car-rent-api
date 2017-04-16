3e6348a64cd858c7887f5cac5178db21fd8fa880
acpbm <acpbm0@gmail.com>
2017-04-15 22:13:46 -0400

    Add validations to vehicle model

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,12 @@
 class Vehicle < ApplicationRecord
   has_many :units, dependent: :destroy
+  validates :manufacturer, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :img, presence: true
+  validates :vehicleType, presence: true
+  validates :capacity, presence: true
+  validates :quantity, presence: true
+  validates :transmission, presence: true
+  validates :rate, presence: true
 end
