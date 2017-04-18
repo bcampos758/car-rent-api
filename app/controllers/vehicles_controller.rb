f2878bcd68ace1d6f3f252f1d2864e221dff8249
acpbm <acpbm0@gmail.com>
2017-04-18 09:36:22 -0400

    Add search and show actions

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,5 +1,14 @@
 class VehiclesController < ApplicationController
   def index
-    render json: Vehicle.all
+    render json: {data: Vehicle.all, status: 200}
   end
+
+  def search
+    render json: { data: Vehicle.joins(:units).where({units: {isAvailable: true, location: params[:location]}}).distinct, status: 200}
+  end
+
+  def show
+    render json: {data: Vehicle.find(params[:id]).units.where(isAvailable: true, location: params[:location]), status: 200}
+  end
+
 end
