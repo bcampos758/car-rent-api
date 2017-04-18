28dfebb83620e27af9572774eabaa84062e02098
acpbm <acpbm0@gmail.com>
2017-04-18 09:37:05 -0400

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
   has_one :account, dependent: :destroy
   has_many :reservations, through: :account
   has_secure_password
