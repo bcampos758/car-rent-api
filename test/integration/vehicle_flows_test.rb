fed1705d071343b61207f90d12b54df73d392dea
acpbm <acpbm0@gmail.com>
2017-04-18 09:42:43 -0400

    Ruby generated test

--- test/integration/vehicle_flows_test.rb
+++ test/integration/vehicle_flows_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class VehicleFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
+
+  test "returns list of all vehicles" do
+    # malke a request to the /vehicles URI
+    get '/vehicles'
+    # the 200 status code means the request has succesed
+    assert_equal 200, response.status, "The request should return status code 200"
+    # 200 response should include the resource in the response body
+    refute_empty response.body, "response.body should not be emplty"
+  end
+
+  # test "returns one vehicle" do
+  #   # malke a request to the /vehicles/:id URI
+  #   get '/vehicles/1'
+  #   # the 200 status code means the request has succesed
+  #   assert_equal 200, response.status, "The request should return status code 200"
   # end
 end
--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class VehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid with all attributes" do
+    vehicle = vehicles(:one)
+    #check Manufacturer is being validated
+    assert vehicle.valid?, "Vehicle was not valid"
+  end
+
+  test "invalid manufacturer gives error message" do
+    vehicle = vehicles(:one)
+    # set manufacturer to nil
+    vehicle.manufacturer = nil
+    #check that the error /can't be blank/ is triggered when the manufacturer attribute is nil
+    vehicle.valid?
+    assert_match /can't be blank/, vehicle.errors[:manufacturer].join, "Presence errors not found on vehicle"
+  end
 end
