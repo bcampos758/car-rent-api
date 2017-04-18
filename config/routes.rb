76afd52d2e6834e39f3e9571342660254ab62f9a
acpbm <acpbm0@gmail.com>
2017-04-18 09:39:09 -0400

    Add routes fro vehicles, users, units

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :vehicles, only: [:index] do
-    get 'search', on: :collection
+  resources :vehicles, only: [:index, :show] do
+    get 'search', 'checkAvalilability',  on: :collection
   end
+
+  resources :units, only: :show do
+    get 'checkAvalilability',  on: :collection
+  end
+  resources :users, only: [:create, :show, :destroy] do
+    resources :units, only: [:update]
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
+
 end
